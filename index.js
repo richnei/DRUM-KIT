108553958ae23a29a4aee93cd3a9f132acc4d97b
Richnei <rich.nei@hotmail.com>
2022-01-17 19:33:44 -0300

    end

--- index.js
+++ index.js
@@ -25,13 +25,28 @@ const tocarSom = (letra) => {
   const audio = new Audio(`./sounds/${sons[letra]}`);
   audio.play();
 };
+
+const adicionarEfeito = (letra) =>
+  document.getElementById(letra).classList.add("active");
+
+const removerEfeito = (letra) => {
+  const div = document.getElementById(letra);
+  const removeActive = () => div.classList.remove("active");
+  div.addEventListener("transitionend", removeActive);
+};
+
 const ativarDiv = (evento) => {
-  const letra = evento.target.id;
+  const letra =
+    evento.type == "click" ? evento.target.id : evento.key.toUpperCase();
   const letraPermitida = sons.hasOwnProperty(letra);
   if (letraPermitida) {
+    adicionarEfeito(letra);
     tocarSom(letra);
+    removerEfeito(letra);
   }
 };
 
 exibir(sons);
 document.getElementById("container").addEventListener("click", ativarDiv);
+
+window.addEventListener("keydown", ativarDiv);
