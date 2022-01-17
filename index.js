ecaef6f77204846115aa542947d40cec213eaad3
Richnei <rich.nei@hotmail.com>
2022-01-17 17:29:56 -0300

    js

---
+++ index.js
@@ -0,0 +1,27 @@
+"use strict";
+
+const sons = {
+  A: "boom.wav",
+  S: "clap.wav",
+  D: "hihat.wav",
+  F: "kick.wav",
+  G: "openhat.wav",
+  H: "ride.wav",
+  J: "snare.wav",
+  K: "tink.wav",
+  L: "tom.wav",
+};
+
+const criarDiv = (texto) => {
+  const div = document.createElement("div");
+  div.classList.add("key");
+  div.textContent = texto;
+  div.id = texto;
+  document.getElementById("container").appendChild(div);
+};
+
+const exibir = (sons) => Object.keys(sons).forEach(criarDiv);
+
+exibir(sons);
+
+add
